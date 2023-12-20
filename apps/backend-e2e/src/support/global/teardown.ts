e2683f67802c095d7867659852ccc040d96cc561
Mendes Hugo <hugo.mendes@noreply.github.com>
2023-12-20 22:26:03 +0100

    chore: add workflows

--- apps/backend-e2e/src/support/global/teardown.ts
+++ apps/backend-e2e/src/support/global/teardown.ts
@@ -4,5 +4,6 @@
 export default function () {
 	// Put clean up logic here (e.g. stopping services, docker-compose, etc.).
 	// Hint: `globalThis` is shared between setup and teardown.
+	// @ts-expect-error -- FIXME
 	console.log(globalThis.__TEARDOWN_MESSAGE__);
 }
