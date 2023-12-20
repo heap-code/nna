f6a59099e7f14e005efbc0e659d827fa37bed055
Mendes Hugo <hugo.mendes@noreply.github.com>
2023-12-20 22:26:03 +0100

    chore: add workflows

--- apps/backend-e2e/jest.config.ts
+++ apps/backend-e2e/jest.config.ts
@@ -1,8 +1,8 @@
 export default {
 	coverageDirectory: "../../coverage/backend-e2e",
 	displayName: "backend-e2e",
-	globalSetup: "<rootDir>/src/support/global-setup.ts",
-	globalTeardown: "<rootDir>/src/support/global-teardown.ts",
+	globalSetup: "<rootDir>/src/support/global/setup.ts",
+	globalTeardown: "<rootDir>/src/support/global/teardown.ts",
 	moduleFileExtensions: ["ts", "js", "html"],
 	preset: "../../jest.preset.js",
 	setupFiles: ["<rootDir>/src/support/test-setup.ts"],
--- apps/office-e2e/cypress.config.ts
+++ apps/office-e2e/cypress.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
 		...(nxE2EPreset(__filename, {
 			cypressDir: "src",
 		}) as Cypress.EndToEndConfigOptions),
-		baseUrl: "http://localhost:4200",
+		baseUrl: "http://localhost:5200",
 	},
 });
