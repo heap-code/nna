f47e6579fc5c1d55a9de0a369d212ab3fa562367
Hugo Mendes <37412330+HugoMendes98@users.noreply.github.com>
2023-12-23 13:43:52 +0000

    x

--- jest.preset.js
+++ jest.preset.js
@@ -5,6 +5,7 @@ const nxPreset = require("@nx/jest/preset").default;
  */
 module.exports = {
 	...nxPreset,
+	coverageReporters: ["html", "json-summary"],
 	watchPlugins: [
 		"jest-watch-typeahead/filename",
 		"jest-watch-typeahead/testname",
