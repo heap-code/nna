5e0fee40e93f68db4274b8518cf38f286f8db95b
Mendes Hugo <mendeshugo98@gmail.com>
2024-02-23 21:20:12 +0100

    pp

--- packages/nest/src/entity/entities/entity.number.ts
+++ packages/nest/src/entity/entities/entity.number.ts
@@ -1,26 +1,27 @@
 import { PrimaryKey, PrimaryKeyOptions } from "@mikro-orm/core";
 import { Type } from "@nestjs/common";
 import { ModelNumber, ModelPrimaryKey } from "@nna/core";
+import { deepmerge } from "deepmerge-ts";
 
 import { EntityCommon, EntityCommonOptions } from "./entity.common";
 
-export type Options<T> = EntityCommonOptions<T> &
+export type Model = ModelNumber.Type;
+
+export type EntityOptions<T> = EntityCommonOptions<T> &
 	Partial<Record<ModelPrimaryKey, PrimaryKeyOptions<T>>>;
 
 /**
  *
  * @param options
  */
-export function EntityNumber<T extends ModelNumber.Type>(
-	options: Options<T> = {},
-): Type<ModelNumber.Type> {
-	const { _id, ...common } = options;
+export function Entity<T extends Model>(
+	options: EntityOptions<T> = {},
+): Type<Model> {
+	const { _id = {}, ...common } = options;
 
-	class EntityNumber
-		extends EntityCommon(common)
-		implements ModelNumber.Type
-	{
-		@PrimaryKey()
+	class EntityNumber extends EntityCommon(common) implements Model {
+		/** Primary key */
+		@PrimaryKey(deepmerge({ autoincrement: true }, _id))
 		public readonly _id!: number;
 	}
 
