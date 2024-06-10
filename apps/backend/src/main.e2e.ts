21e62df76bb84779e3b65581fd0e42d0bd0227ab
Mendes Hugo <37412330+HugoMendes98@users.noreply.github.com>
2024-06-10 09:26:39 +0000

    chore: backend e2e

--- apps/backend/src/main.e2e.ts
+++ apps/backend/src/main.e2e.ts
@@ -1,5 +1,6 @@
 import { LogLevel, Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import * as z from "zod";
 
 import { AppModule } from "./app/app.module";
 import { bootstrap } from "./bootstrap";
@@ -17,10 +18,23 @@ void (async () => {
 			app: { name: __APP_NAME__, version: `${__APP_VERSION__}-e2e` },
 			auth: {
 				cookie: { name: "e2e-cookie" },
-				duration: 30 * 60,
+				duration: 10 * 60,
 				secret: "e2e-secret",
 			},
+			host: {
+				cors: { origin: /\/\/localhost(:\d{1,5})+/ },
+				globalPrefix: "/e2e/api",
+				name: "127.0.0.1",
+				port: 33000,
+			},
 			logger,
+			orm: {
+				dbName: z
+					.string()
+					.min(1)
+					.default("db-e2e")
+					.parse(process.env.BE_DB_TEST_NAME),
+			},
 			swagger: false,
 		}),
 		{ logger },
@@ -30,6 +44,6 @@ void (async () => {
 
 	await app.listen(host.port, host.name);
 	Logger.debug(
-		`🧪 E2E[${APP_NAME} - v${APP_VERSION}] is running on: ${await app.getUrl()}`,
+		`💉 App-e2e 🧪 [${APP_NAME} - v${APP_VERSION}] is running on: ${await app.getUrl()}`,
 	);
 })();
