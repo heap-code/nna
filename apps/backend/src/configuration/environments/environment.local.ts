9a4c01377e13550e5d439dfd0f44e1078b3b032c
Hugo Mendes <37412330+HugoMendes98@users.noreply.github.com>
2024-08-21 17:11:07 +0200

    chore: add updates (#577)

--- apps/backend/src/configuration/environments/environment.local.ts
+++ apps/backend/src/configuration/environments/environment.local.ts
@@ -1,4 +1,5 @@
 import { deepmergeCustom } from "deepmerge-ts";
+import { PartialDeep } from "type-fest";
 
 import { ENVIRONMENT_DEFAULT } from "./environment.default";
 import { Environment } from "./environment.interface";
@@ -7,5 +8,6 @@ import { MY_ENVIRONMENT } from "../../environment";
 /** This export the default environment with local overrides */
 export const ENVIRONMENT: Environment = deepmergeCustom({ mergeArrays: false })(
 	ENVIRONMENT_DEFAULT,
+	{ swagger: true } satisfies PartialDeep<Environment>,
 	MY_ENVIRONMENT as Environment,
 );
