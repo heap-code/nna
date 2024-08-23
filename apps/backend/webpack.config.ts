a864e0f3f319b005b39150cdd0a4f7be094b0aee
Hugo Mendes <37412330+HugoMendes98@users.noreply.github.com>
2024-08-23 16:06:06 +0200

    chore: updates (#608)
    * chore: enable swc for backend
    
    * chore: separate backend typechecking (for faster build)

--- apps/backend/webpack.config.ts
+++ apps/backend/webpack.config.ts
@@ -1,4 +1,5 @@
-import { composePlugins, withNx } from "@nx/webpack";
+import { composePlugins } from "@nx/webpack";
+import { NxAppWebpackPlugin } from "@nx/webpack/app-plugin";
 import { deepmerge } from "deepmerge-ts";
 import * as GeneratePackageJsonPlugin from "generate-package-json-webpack-plugin";
 import type * as handlebars from "handlebars";
@@ -6,60 +7,79 @@ import { minify } from "html-minifier";
 import { DefinePlugin } from "webpack";
 
 // eslint-disable-next-line @nx/enforce-module-boundaries -- The bundler is not "part" of the app
-import { name, version } from "../../package.json";
+import * as packageJson from "../../package.json";
+
+const { name, version } = packageJson;
 
 // Nx plugins for webpack.
-export default composePlugins(withNx({ target: "node" }), (config, ctx) =>
-	deepmerge(config, {
-		module: {
-			rules: [
-				{
-					test: [/\.handlebars$/, /\.hbs$/],
-					use: [
-						{
-							loader: "handlebars-loader",
-							options: {
-								// https://www.npmjs.com/package/handlebars-loader#details
-								debug: false,
-								precompileOptions: {
-									preventIndent: true,
-									// Raise error on non-production build
-									//	prefer incomplete emails over no email sent or 500 errors
-									strict: ctx.configuration !== "production",
-								} satisfies Parameters<
-									typeof handlebars.precompile
-								>[1],
-							},
-						},
-						{
-							loader: "string-replace-loader",
-							options: {
-								flags: "gm",
-								replace: content =>
-									minify(content, {
-										collapseWhitespace: true,
-										minifyCSS: true,
-										minifyJS: true,
-										minifyURLs: true,
-										removeComments: true,
-										sortAttributes: true,
-									}),
-								search: /((.|\s)*)/gm,
-							},
-						},
-					],
-				},
-			],
-		},
+export default composePlugins(
+	(config, { options }) => ({
+		...config,
 		plugins: [
-			new DefinePlugin({
-				__APP_NAME__: JSON.stringify(name),
-				__APP_VERSION__: JSON.stringify(version),
+			...(config.plugins ?? []),
+			// Typechecking is not run on build (run on serve/watch and verified on CI)
+			new NxAppWebpackPlugin({
+				compiler: "swc",
+				/** The {@link GeneratePackageJsonPlugin} plugin is a bit better => it takes what is bundled (even devDep for e2e) */
+				generatePackageJson: false,
+				outputHashing: false,
+				skipTypeChecking: !options.watch,
+				target: "node",
 			}),
-			new GeneratePackageJsonPlugin(
-				{},
-				{ useInstalledVersions: true },
-			) as never,
 		],
-	} satisfies typeof config),
+		watch: options.watch,
+	}),
+	(config, { configuration }) =>
+		deepmerge(config, {
+			module: {
+				rules: [
+					{
+						test: [/\.handlebars$/, /\.hbs$/],
+						use: [
+							{
+								loader: "handlebars-loader",
+								options: {
+									// https://www.npmjs.com/package/handlebars-loader#details
+									debug: false,
+									precompileOptions: {
+										preventIndent: true,
+										// Raise error on non-production build
+										//	prefer incomplete emails over no email sent or 500 errors
+										strict: configuration !== "production",
+									} satisfies Parameters<
+										typeof handlebars.precompile
+									>[1],
+								},
+							},
+							{
+								loader: "string-replace-loader",
+								options: {
+									flags: "gm",
+									replace: content =>
+										minify(content, {
+											collapseWhitespace: true,
+											minifyCSS: true,
+											minifyJS: true,
+											minifyURLs: true,
+											removeComments: true,
+											sortAttributes: true,
+										}),
+									search: /((.|\s)*)/gm,
+								},
+							},
+						],
+					},
+				],
+			},
+			plugins: [
+				new DefinePlugin({
+					__APP_NAME__: JSON.stringify(name),
+					__APP_VERSION__: JSON.stringify(version),
+				}),
+				new GeneratePackageJsonPlugin(
+					{ name, version } satisfies Partial<typeof packageJson>,
+					{ useInstalledVersions: true },
+				) as never,
+			],
+		} satisfies typeof config),
 );
