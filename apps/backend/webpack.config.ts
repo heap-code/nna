97ea0bbe6403c3cb13a4face9b73da8e555bb540
Mendes Hugo <37412330+HugoMendes98@users.noreply.github.com>
2024-08-23 07:16:42 +0000

    chore: enable swc for backend

--- apps/backend/webpack.config.ts
+++ apps/backend/webpack.config.ts
@@ -1,6 +1,6 @@
-import { composePlugins, withNx } from "@nx/webpack";
+import { composePlugins } from "@nx/webpack";
+import { NxAppWebpackPlugin } from "@nx/webpack/app-plugin";
 import { deepmerge } from "deepmerge-ts";
-import * as GeneratePackageJsonPlugin from "generate-package-json-webpack-plugin";
 import type * as handlebars from "handlebars";
 import { minify } from "html-minifier";
 import { DefinePlugin } from "webpack";
@@ -9,57 +9,67 @@ import { DefinePlugin } from "webpack";
 import { name, version } from "../../package.json";
 
 // Nx plugins for webpack.
-export default composePlugins(withNx({ target: "node" }), (config, ctx) =>
-	deepmerge(config, {
-		module: {
-			rules: [
-				{
-					test: [/\.handlebars$/, /\.hbs$/],
-					use: [
-						{
-							loader: "handlebars-loader",
-							options: {
-								// https://www.npmjs.com/package/handlebars-loader#details
-								debug: false,
-								precompileOptions: {
-									preventIndent: true,
-									// Raise error on non-production build
-									//	prefer incomplete emails over no email sent or 500 errors
-									strict: ctx.configuration !== "production",
-								} satisfies Parameters<
-									typeof handlebars.precompile
-								>[1],
-							},
-						},
-						{
-							loader: "string-replace-loader",
-							options: {
-								flags: "gm",
-								replace: content =>
-									minify(content, {
-										collapseWhitespace: true,
-										minifyCSS: true,
-										minifyJS: true,
-										minifyURLs: true,
-										removeComments: true,
-										sortAttributes: true,
-									}),
-								search: /((.|\s)*)/gm,
-							},
-						},
-					],
-				},
-			],
-		},
+export default composePlugins(
+	config => ({
+		...config,
 		plugins: [
-			new DefinePlugin({
-				__APP_NAME__: JSON.stringify(name),
-				__APP_VERSION__: JSON.stringify(version),
+			...(config.plugins ?? []),
+			new NxAppWebpackPlugin({
+				compiler: "swc",
+				generatePackageJson: true,
+				outputHashing: false,
+				target: "node",
 			}),
-			new GeneratePackageJsonPlugin(
-				{},
-				{ useInstalledVersions: true },
-			) as never,
 		],
-	} satisfies typeof config),
+	}),
+	(config, ctx) =>
+		deepmerge(config, {
+			module: {
+				rules: [
+					{
+						test: [/\.handlebars$/, /\.hbs$/],
+						use: [
+							{
+								loader: "handlebars-loader",
+								options: {
+									// https://www.npmjs.com/package/handlebars-loader#details
+									debug: false,
+									precompileOptions: {
+										preventIndent: true,
+										// Raise error on non-production build
+										//	prefer incomplete emails over no email sent or 500 errors
+										strict:
+											ctx.configuration !== "production",
+									} satisfies Parameters<
+										typeof handlebars.precompile
+									>[1],
+								},
+							},
+							{
+								loader: "string-replace-loader",
+								options: {
+									flags: "gm",
+									replace: content =>
+										minify(content, {
+											collapseWhitespace: true,
+											minifyCSS: true,
+											minifyJS: true,
+											minifyURLs: true,
+											removeComments: true,
+											sortAttributes: true,
+										}),
+									search: /((.|\s)*)/gm,
+								},
+							},
+						],
+					},
+				],
+			},
+			plugins: [
+				new DefinePlugin({
+					__APP_NAME__: JSON.stringify(name),
+					__APP_VERSION__: JSON.stringify(version),
+				}),
+			],
+		} satisfies typeof config),
 );
