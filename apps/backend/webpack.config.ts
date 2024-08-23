ad6175abb2ed9d9a7733666fe85c3939332a28fb
Mendes Hugo <37412330+HugoMendes98@users.noreply.github.com>
2024-08-23 13:22:41 +0000

    chore: separate backend typechecking (for faster build)

--- apps/backend/webpack.config.ts
+++ apps/backend/webpack.config.ts
@@ -1,28 +1,35 @@
 import { composePlugins } from "@nx/webpack";
 import { NxAppWebpackPlugin } from "@nx/webpack/app-plugin";
 import { deepmerge } from "deepmerge-ts";
+import * as GeneratePackageJsonPlugin from "generate-package-json-webpack-plugin";
 import type * as handlebars from "handlebars";
 import { minify } from "html-minifier";
 import { DefinePlugin } from "webpack";
 
 // eslint-disable-next-line @nx/enforce-module-boundaries -- The bundler is not "part" of the app
-import { name, version } from "../../package.json";
+import * as packageJson from "../../package.json";
+
+const { name, version } = packageJson;
 
 // Nx plugins for webpack.
 export default composePlugins(
-	config => ({
+	(config, { options }) => ({
 		...config,
 		plugins: [
 			...(config.plugins ?? []),
+			// Typechecking is not run on build (run on serve/watch and verified on CI)
 			new NxAppWebpackPlugin({
 				compiler: "swc",
-				generatePackageJson: true,
+				/** The {@link GeneratePackageJsonPlugin} plugin is a bit better => it takes what is bundled (even devDep for e2e) */
+				generatePackageJson: false,
 				outputHashing: false,
+				skipTypeChecking: !options.watch,
 				target: "node",
 			}),
 		],
+		watch: options.watch,
 	}),
-	(config, ctx) =>
+	(config, { configuration }) =>
 		deepmerge(config, {
 			module: {
 				rules: [
@@ -38,8 +45,7 @@ export default composePlugins(
 										preventIndent: true,
 										// Raise error on non-production build
 										//	prefer incomplete emails over no email sent or 500 errors
-										strict:
-											ctx.configuration !== "production",
+										strict: configuration !== "production",
 									} satisfies Parameters<
 										typeof handlebars.precompile
 									>[1],
@@ -70,6 +76,10 @@ export default composePlugins(
 					__APP_NAME__: JSON.stringify(name),
 					__APP_VERSION__: JSON.stringify(version),
 				}),
+				new GeneratePackageJsonPlugin(
+					{ name, version } satisfies Partial<typeof packageJson>,
+					{ useInstalledVersions: true },
+				) as never,
 			],
 		} satisfies typeof config),
 );
