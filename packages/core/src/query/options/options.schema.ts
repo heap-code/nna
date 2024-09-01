95258454def370f3b55d0362009354018b6fb649
Mendes Hugo <37412330+HugoMendes98@users.noreply.github.com>
2024-09-01 14:30:36 +0000

    chore: set default limit for `QueryObject`

--- packages/core/src/query/options/options.schema.ts
+++ packages/core/src/query/options/options.schema.ts
@@ -4,8 +4,20 @@ import type { QueryObjectSchema, QueryOptions } from "..";
 import { FilterOptions } from "../filter";
 import * as QueryOrder from "../order";
 
+/** Default limit set to schema */
+export const QUERY_OPTIONS_DEFAULT_LIMIT = 100;
+
 /** Options to create a query options validation schema */
-export type QueryOptionsOptions = Pick<FilterOptions, "coerce" | "strict">;
+export interface QueryOptionsOptions
+	extends Pick<FilterOptions, "coerce" | "strict"> {
+	/**
+	 * Default value for `limit` property.
+	 *	`null` to not set any.
+	 *
+	 * @default QUERY_OPTIONS_LIMIT_DEFAULT
+	 */
+	defaultLimit?: number | null;
+}
 
 /**
  * Creates a {@link Options query options} validation schema for an object schema.
@@ -25,16 +37,20 @@ function _schema<T extends QueryObjectSchema>(
 	type QryOptions = QueryOptions<z.infer<T>>;
 	type QryOptShape = Record<keyof QryOptions, z.ZodType>;
 
-	const { coerce = false } = options;
-	const numberSchema = z.number({ coerce }).gte(0);
+	const { coerce = false, defaultLimit = QUERY_OPTIONS_DEFAULT_LIMIT } =
+		options;
+	const numberSchema = z.number({ coerce }).gte(0).optional();
 
-	return z
-		.object({
-			limit: numberSchema,
-			order: z.array(z.lazy(() => QueryOrder.order(schema, options))),
-			skip: numberSchema,
-		} satisfies QryOptShape)
-		.partial() satisfies z.ZodType<QryOptions>;
+	return z.object({
+		limit:
+			defaultLimit === null
+				? numberSchema
+				: numberSchema.default(defaultLimit),
+		order: z
+			.array(z.lazy(() => QueryOrder.order(schema, options)))
+			.optional(),
+		skip: numberSchema,
+	} satisfies QryOptShape);
 }
 
 export { _schema as options };
