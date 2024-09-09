5b0c42c7d27478dbc4d16dcc148436c529d02109
Mendes Hugo <37412330+HugoMendes98@users.noreply.github.com>
2024-09-09 19:49:28 +0000

    chore: chore: normalize use of swc in tests

--- packages/nest/jest.config.ts
+++ packages/nest/jest.config.ts
@@ -1,5 +1,26 @@
-import * as swc from "@swc/core";
-import { JestConfigWithTsJest } from "ts-jest";
+import type * as swc from "@swc/core";
+import * as fs from "fs";
+import type { JestConfigWithTsJest } from "ts-jest";
+
+const swcConfig = JSON.parse(
+	fs.readFileSync(`${__dirname}/.swcrc.json`, "utf-8"),
+) as swc.Options;
+
+const swcJestConfig = {
+	...swcConfig,
+
+	// Reading the SWC compilation config and remove the "exclude"
+	// for the test files to be compiled by SWC
+	exclude: [],
+	// disable .swcrc look-up by SWC core because we're passing in swcJestConfig ourselves.
+	// If we do not disable this, SWC Core will read .swcrc and won't transform our test files due to "exclude"
+	swcrc: false,
+} satisfies swc.Options;
+
+// Uncomment if using global setup/teardown files being transformed via swc
+// https://nx.dev/packages/jest/documents/overview#global-setup/teardown-with-nx-libraries
+// jest needs EsModule Interop to find the default exported setup/teardown functions
+// swcJestConfig.module.noInterop = false;
 
 export default {
 	displayName: "nest",
@@ -7,27 +28,6 @@ export default {
 	preset: "../../jest.preset.js",
 	testEnvironment: "node",
 	transform: {
-		"^.+\\.[tj]s$": [
-			"@swc/jest",
-			{
-				jsc: {
-					externalHelpers: true,
-					keepClassNames: true,
-					loose: true,
-					parser: {
-						decorators: true,
-						dynamicImport: true,
-						syntax: "typescript",
-					},
-					target: "es2021",
-					transform: {
-						decoratorMetadata: true,
-						legacyDecorator: true,
-					},
-				},
-				module: { type: "commonjs" },
-				sourceMaps: "inline",
-			} satisfies swc.Options,
-		],
+		"^.+\\.[tj]s$": ["@swc/jest", swcJestConfig],
 	},
 } satisfies JestConfigWithTsJest;
