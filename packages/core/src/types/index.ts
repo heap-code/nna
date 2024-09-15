091f902607e8a96efb410bf443ca5e874b20b4af
Mendes Hugo <37412330+HugoMendes98@users.noreply.github.com>
2024-09-15 20:13:19 +0000

    chore: add package.json command

--- packages/core/src/types/index.ts
+++ packages/core/src/types/index.ts
@@ -2,4 +2,5 @@ export * from "./extend-functions";
 export * from "./function";
 export * from "./keep-in-array-when";
 export * from "./keep-properties-of";
+export * from "./omit-known";
 export * from "./unreflected";
